b5899bcfcfeee4f9a9e9a942eb1c0e0d6a1aabb5
sMailund <simen.m.s@hotmail.com>
2020-04-11 00:15:13 +0200

    create solution submission endpoint

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import datetime
 from google.cloud import datastore
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 datastore_client = datastore.Client()
 
 app = Flask(__name__)
 
-
-@app.route('/task')
-def get():
-    query = datastore_client.query(kind='task')#.add_filter("task_number", "=", "1")
+# TODO: error handling
+def get_task(task_number):
+    query = datastore_client.query(kind='task').add_filter("task_number", "=", int(task_number))
     result = list(query.fetch())
 
-    return jsonify(result)
+    return result[0]
+
+
+@app.route('/task/<task_number>', methods=["GET"])
+def get(task_number):
+    result = get_task(task_number)
+    return jsonify(result), 200
+
+@app.route('/task/<task_number>', methods=["PATCH"])
+def patch(task_number):
+    task = get_task(task_number)
+    reset = request.form.get("reset")
+    if reset:
+        task["solved"] = False
+        return task, 200
+    answer = request.form.get("answer").strip().lower()
+    if (task["solution"]) == answer:
+        task["solved"] = True
+        datastore_client.put(task)
+        return task, 200
+    else:
+        return task, 400 # TODO: is there a cleaner rest way to do this?
 
 
 if __name__ == '__main__':
--- readme.md
+++ readme.md
@@ -1,6 +1,13 @@
 # project brass backend
 Simple ctf-like platform for irl treasure hunt. (don't let my friend know I'm planning his surprise birthday party)
 
+## about
+Small project made for a friends surpise birthday party.
+Project was limited in scope and 
+only meant to be used a single day by a single user, 
+so few resources were invested in 
+architecture, robustness and security.
+
 ## usage
 ### run for local development
 #### export gcp credentials
