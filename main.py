e674874b01c153ae8b6e4ef32fea2bf3cd87c66b
sMailund <simen.m.s@hotmail.com>
2020-04-11 00:24:07 +0200

    create list endpoint

--- main.py
+++ main.py
@@ -9,31 +9,37 @@ app = Flask(__name__)
 
 # TODO: error handling
 def get_task(task_number):
-    query = datastore_client.query(kind='task').add_filter("task_number", "=", int(task_number))
+    query = datastore_client.query(kind='task').add_filter('task_number', '=', int(task_number))
     result = list(query.fetch())
 
     return result[0]
 
-
-@app.route('/task/<task_number>', methods=["GET"])
+@app.route('/task/<task_number>', methods=['GET'])
 def get(task_number):
     result = get_task(task_number)
     return jsonify(result), 200
 
-@app.route('/task/<task_number>', methods=["PATCH"])
+@app.route('/task/<task_number>', methods=['PATCH'])
 def patch(task_number):
     task = get_task(task_number)
-    reset = request.form.get("reset")
+    reset = request.form.get('reset')
     if reset:
-        task["solved"] = False
+        task['solved'] = False
         return task, 200
-    answer = request.form.get("answer").strip().lower()
-    if (task["solution"]) == answer:
-        task["solved"] = True
+    answer = request.form.get('answer').strip().lower()
+    if (task['solution']) == answer:
+        task['solved'] = True
         datastore_client.put(task)
-        return task, 200
+        return jsonify(task), 200
     else:
-        return task, 400 # TODO: is there a cleaner rest way to do this?
+        return jsonify(task), 400 # TODO: is there a cleaner rest way to do this?
+
+@app.route('/task/', methods=['GET'])
+def list_tasks():
+    query = datastore_client.query(kind='task')
+    query.order = ['task_number']
+    result = list(query.fetch())
+    return jsonify(result), 200
 
 
 if __name__ == '__main__':
